3cbae908a3ba8cb510da073e0cfd636edea9388c
zainabaliammarali-cloud <zainabaliammarali@gmail.com>
2026-08-06 20:57:55 +0300

    Notification Model

---
+++ models/Notification.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    reciver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    type: {
+      type: String,
+      enum: ["like", "comment", "follow"],
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  },
+  { timestamps: true },
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+module.exports = Notification;
