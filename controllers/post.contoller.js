c890fb017e2fe27d3d8b57fb353d70fb84dd74a1
Your Name <you@example.com>
2026-08-07 17:49:23 +0300

    Post Controller

---
+++ controllers/post.contoller.js
@@ -0,0 +1,85 @@
+const Post = require('../models/Post')
+
+async function createNewPost(req, res) {
+    try {
+        const { caption, image, category } = req.body
+
+        const createdPost = await Post.create({
+            user: req.user._id,
+            caption, image, category
+        })
+
+        res.status(201).json(createdPost)
+    } catch (e) {
+        res.status(500).json({ message: e.message })
+    }
+}
+
+
+async function getAllPosts(req, res) {
+    try {
+        const allPosts = await Post.find()
+
+        res.status(200).json(allPosts)
+    } catch (e) {
+        res.status(500).json({ message: e.message })
+    }
+}
+
+
+async function getPostById(req, res) {
+    try {
+        const onePost = await Post.findById(req.params.id)
+
+        if (!onePost) {
+            return res.status(404).json({ message: "Post Not Found" })
+        }
+
+        res.status(200).json(onePost)
+    } catch (e) {
+        res.status(500).json({ message: e.message })
+    }
+}
+
+
+async function updatePostById(req, res) {
+    try {
+        const { caption, image, category } = req.body
+
+        const updatedPost = await Post.findByIdAndUpdate(req.params.id,
+            { caption, image, category },
+            { new: true, runValidators: true })
+
+        if (!updatedPost) {
+            return res.status(404).json({ message: "Post Not Found" })
+        }
+
+        res.status(200).json(updatedPost)
+    } catch (e) {
+        res.status(500).json({ message: e.message })
+    }
+}
+
+
+
+async function deletePostById(req, res) {
+    try {
+        const deletedPost = await Post.findByIdAndDelete(req.params.id)
+
+    if (!deletedPost) {
+      return res.status(404).json({ message: "Cannot Delete Post" });
+    }
+
+    res.status(200).json(deletedPost)
+    } catch (e) {
+        res.status(500).json({ message: e.message })
+    }
+}
+
+module.exports = {
+    createNewPost,
+    getAllPosts,
+    getPostById,
+    updatePostById,
+    deletePostById
+}
