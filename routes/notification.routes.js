65c35b4fb5c8bcf32304e4779e6d879052c2eb3c
zainabaliammarali-cloud <zainabaliammarali@gmail.com>
2026-08-09 17:24:45 +0300

    remove create  notification

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -3,6 +3,5 @@ const verifyToken = require("../middleware/verifyToken");
 const NotificationController = require("../controllers/notification.controller");
 
 router.get("/", verifyToken, NotificationController.getNotifications);
-router.post("/", verifyToken, NotificationController.createNotification);
 
 module.exports = router;
